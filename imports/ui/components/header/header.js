32f193733867061f3e9696db19341efc31ba8711
Pramveer Rothan <pramveer1010@gmail.com>
2018-05-23 22:39:08 +0530

    Fixed the click issue

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -1,1 +1,23 @@
 import './header.html';
+
+
+
+Template.Header.onCreated(function() {
+
+});
+
+Template.Header.rendered = () => {
+
+};
+
+Template.Header.helpers({
+
+});
+
+Template.Header.events({
+    'click #sidebarCollapse': () => {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    }
+});
