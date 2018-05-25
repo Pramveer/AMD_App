b3cc8f3756fe25a4c863447030191e844b6ef5ab
prothan <pramveer1010@gmail.com>
2018-05-25 12:32:14 +0530

    Added the user info

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -1,5 +1,7 @@
+import '../userInfoModal';
 import './header.html';
 
+// jQuery.noConflict();
 
 
 Template.Header.onCreated(function() {
@@ -11,6 +13,24 @@ Template.Header.rendered = () => {
 };
 
 Template.Header.helpers({
+    renderDatepicker : () => {
+        let start = new Date('01/01/2013');
+        let end = new Date();
+
+        $('#timeFilterDistSelect').daterangepicker({
+            startDate: start,
+            endDate: end,
+            parentEl: '#dv-dash-time-filter',
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        });
+    }
 
 });
 
@@ -23,5 +43,48 @@ Template.Header.events({
 		    // renderhepetitiesChart(containerchart,data1);
 			Template.Dashboard.ReRenderCharts();
 		    },50);
+    },
+
+    'click .dropdown-menu': (e) => {
+        // console.log(e.target)
+        // console.log(e.target.className)
+        if (e.target.className === 'search-criteria' || e.target.className === 'caret') {
+            $('.subSearchDropdown a').not($(e.target).parent()).next('ul').hide();
+            $(e.target).parent().next('ul').toggle();
+        } else {
+            $('.subSearchDropdown a').not(e.target).next('ul').hide();
+            $(e.target).next('ul').toggle();
+        }    
+        e.stopPropagation();
+        e.preventDefault();
+    },
+    'click .subSearchTab a[data-toggle="tab"]': (e) => {
+        $('.subSearchDropdown a').next('ul').hide();
+        console.log(e.target)
+        console.log(this)
+        e.stopPropagation();
+        e.preventDefault();
+        $(e.target).tab('show');
+    },
+
+    'click .js-dashboard-applyDateFilters': (event, template) => {
+        
+        let start = new Date('01/01/2013');
+        let end = new Date();
+        $('#timeFilterDistSelect').daterangepicker({
+            startDate: start,
+            endDate: end,
+            parentEl: '#dv-dash-time-filter',
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
+        });
+        event.stopPropagation();
+        // event.preventDefault();
     }
 });
---
+++ imports/ui/components/userInfoModal/index.js
@@ -0,0 +1,1 @@
+import './userInfoModal.js';
